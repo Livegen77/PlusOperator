69a0daaa46ed4edb0865a9747658cb3957402295
Your Name <you@example.com>
2018-02-24 20:57:02 +0100

    change variable name from french to english

--- main.c
+++ main.c
@@ -5,7 +5,7 @@ unsigned int plusoperator(const unsigned int first, const unsigned int second){
 	unsigned int length_unsigned_int = 32;
 	unsigned int bit_index = 0;
 	unsigned int result = 0;
-	unsigned int retenu = 0;
+	unsigned int carry = 0;
 
 	/* Go through each byte of the two numbers */
 	for(bit_index = 0; bit_index < length_unsigned_int; bit_index++){
@@ -13,17 +13,17 @@ unsigned int plusoperator(const unsigned int first, const unsigned int second){
 
 		/* Bit in both value match */
 		if((first & value_bit) && (second &value_bit)){
-			if(retenu){
+			if(carry){
 				result |= value_bit;
-				retenu = 1;
+				carry = 1;
 			} else {
-				retenu = 1;
+				carry = 1;
 			}
 		}
 		/* Bit only active on the first number */	
 		else if(first & value_bit){
-			if(retenu){
-				retenu = 0;
+			if(carry){
+				carry = 0;
 			}
 			else {
 				result |= value_bit;
@@ -31,8 +31,8 @@ unsigned int plusoperator(const unsigned int first, const unsigned int second){
 		}
 		/* Bit only active on the second number */
 		else if(second & value_bit){
-			if(retenu){
-				retenu = 0;
+			if(carry){
+				carry = 0;
 			}
 			else {
 				result |= value_bit;
@@ -40,9 +40,9 @@ unsigned int plusoperator(const unsigned int first, const unsigned int second){
 		}
 		/* Bit not used by any numbers */ 
 		else{
-			if(retenu){
+			if(carry){
 				result |= value_bit;
-				retenu = 0;
+				carry = 0;
 			}
 		}	
 	}
@@ -66,6 +66,6 @@ void main(int argc, char **argv){
 	expectedresult = first + second;
 	calculatedresult = plusoperator(first, second);
 
-	printf("expected result = %i \n", expectedresult);
+	printf("expected result = %u \n", expectedresult);
 	printf("calculated result = %u \n", calculatedresult); 
 }
