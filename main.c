4204270d88b7b767c64193bf9d030ad63c81a866
Your Name <you@example.com>
2018-02-24 20:50:09 +0100

    correction when both bits match

--- main.c
+++ main.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
-int plusoperator(const unsigned int first, const unsigned int second){
+unsigned int plusoperator(const unsigned int first, const unsigned int second){
 
 	unsigned int length_unsigned_int = 32;
 	unsigned int bit_index = 0;
 	unsigned int result = 0;
 	unsigned int retenu = 0;
 
+	/* Go through each byte of the two numbers */
 	for(bit_index = 0; bit_index < length_unsigned_int; bit_index++){
 		unsigned int value_bit = (1 << bit_index);
 
@@ -14,11 +15,12 @@ int plusoperator(const unsigned int first, const unsigned int second){
 		if((first & value_bit) && (second &value_bit)){
 			if(retenu){
 				result |= value_bit;
-				retenu = 0;
+				retenu = 1;
 			} else {
 				retenu = 1;
 			}
-		}	
+		}
+		/* Bit only active on the first number */	
 		else if(first & value_bit){
 			if(retenu){
 				retenu = 0;
@@ -27,6 +29,7 @@ int plusoperator(const unsigned int first, const unsigned int second){
 				result |= value_bit;
 			}
 		}
+		/* Bit only active on the second number */
 		else if(second & value_bit){
 			if(retenu){
 				retenu = 0;
@@ -35,6 +38,7 @@ int plusoperator(const unsigned int first, const unsigned int second){
 				result |= value_bit;
 			}
 		}
+		/* Bit not used by any numbers */ 
 		else{
 			if(retenu){
 				result |= value_bit;
@@ -53,17 +57,11 @@ void main(int argc, char **argv){
 	unsigned int 	calculatedresult = 0;
 	unsigned char 	operator = 0;
 
-	while((operator != '+') && (operator != '-')){
-		printf("+ or minus - ? %c \n", operator);
-		scanf("%c", &operator);
-	}
-
 	puts("number 1 ?");
-	scanf("%i", &first)
-;
+	scanf("%i", &first);
+
 	puts("number 2 ?");
 	scanf("%i", &second);
-
 
 	expectedresult = first + second;
 	calculatedresult = plusoperator(first, second);
